1c4711d2b4f2bca2dd18a72aa951b6cbc0fd357f
WaelLABASSI <47293446+WaelLABASSI@users.noreply.github.com>
2023-10-11 15:56:06 +0200

    Update flask application

--- version_1/application.py
+++ version_1/application.py
@@ -1,20 +1,25 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 import joblib
 application = Flask(__name__)
 
 vectorizer=joblib.load("vectorizer.pkl")
 spam_ham_model=joblib.load("spam_ham_model.pkl")
 
-@application.route('/')
-def hello_world():
-    return "Welcome to spam or ham application"
 
-@application.route('/spamorham', methods=['GET', 'POST'])
+@application.route('/', methods=['GET', 'POST'])
 def spamorham():
-    message = request.args.get("message")
-    vect_message = vectorizer.transform([message])
-    result = spam_ham_model.predict(vect_message)[0]
-    return result
+    if request.method == "POST":
+        #Get sent SMS
+        message = request.form.get('message')
+        
+        #Predict result
+        vect_message = vectorizer.transform([message])
+        result = spam_ham_model.predict(vect_message)[0]
+
+        print(result)
+        return render_template("index.html", result= result)
+    else:
+        return render_template("index.html")
 
 if __name__ == '__main__':
     application.run()
